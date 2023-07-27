269bbf8653958b02c48cd41a475e72dc90902566
Diony Castro <11267927+dnycstr@users.noreply.github.com>
2023-07-27 05:23:00 -0700

    Initial Commit (#1)

---
+++ src/modules/billing/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="text-center py-10">
+        <span className="text-3xl">Welcome to Billing Module</span>
+      </div>
+    </>
+  );
+};
